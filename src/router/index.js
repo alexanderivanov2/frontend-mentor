bdb4ef6b3a064dce27986d5ad74d14ada86d0e7c
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-10-17 23:18:39 +0300

    change-route to QR code card

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import NewbieQRCode from '../views/NewbiePages/NewbieQRCode.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/newbie/QR-code',
       name: 'NewbieQRCode',
-      component: () => import('../views/NewbiePages/NewbieQRCode.vue')
+      component: NewbieQRCode
     }
     // {
     //   path: '/about',
