3fd5d67b91803330b209126655b3bc14eaaf0123
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-10-18 23:55:04 +0300

    implement new home page card style and home page layout adjustment

--- src/components/homePage/HomePageCard.vue
+++ src/components/homePage/HomePageCard.vue
@@ -1,32 +1,50 @@
 <template>
     <div class="home-page-card">
-        <h6 class="home-page-card-title">QR Code Component</h6>
-        <router-link :to="{ name: 'NewbieQRCode'}" class="home-page-card-link">QR Code &#187;</router-link>
+        <h6 class="home-page-card-title">{{ title }}</h6>
+        <router-link :to="{ name: routeName }" class="home-page-card-link">{{ title }} &#187;</router-link>
     </div>
 </template>
+
+<script setup>
+    import { toRefs } from 'vue'
+    
+    const props = defineProps({
+        title: {
+            type: String,
+            default: '',
+        },
+        routeName: {
+            type: String,
+            default: 'Home',
+        },
+    })
+
+    const { title, routeName } = toRefs(props)
+
+</script>
 
 <style lang="scss">
     .home-page-card {
         max-width: 250px;
         max-height: 250px;
-        border: 1px solid #D3D0CB;
+        border: 3px solid #1D2C35;
         border-radius: 10px;
 
         .home-page-card-title {
             padding: 1rem;
             background-color: white;
-            border-bottom: 3px solid #153A51;
-            border-top-left-radius: 8px;
-            border-top-right-radius: 8px;
+            border-bottom: 5px solid #F4CB15;
+            border-top-left-radius: 6px;
+            border-top-right-radius: 6px;
             text-align: center;
             font-weight: bold;
         }
 
         .home-page-card-link {
             display: block;
-            background-color: #016FB9;
-            border-bottom-left-radius: 10px;
-            border-bottom-right-radius: 10px;
+            background-color: #05050F;
+            border-bottom-left-radius: 6px;
+            border-bottom-right-radius: 6px;
             padding: 1rem;
             text-align: center;
             text-decoration: none;
@@ -34,7 +52,7 @@
             font-weight: bold;
 
             &:hover {
-                background-color: #34ADFE;
+                background-color: #F26419;
                 color: black;
                 transition: all ease-in-out 500ms;
             }
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,51 @@
 <template>
     <div class="home-page">
-        <HomePageCard />
+        <h2 class="home-page-challange-title">Newbie Challanges</h2>
+        <div class="home-page-newbie-cards-wrapper">
+            <HomePageCard v-for="card in newbieCardsList" 
+                :key="card.cardName"
+                :title="card.cardName"
+                :routeName="card.routeName"
+            />
+        </div>
     </div>
 </template>
 
 <script setup>
+import { newbieCardsList } from '../assets/constants/homePageConstants/newbieCardsConstants' 
 import HomePageCard from '../components/homePage/HomePageCard.vue'
 
 </script>
+
+<style lang="scss" scoped>
+.home-page {
+    @media (max-width: 475px) {
+        .home-page-challange-title {
+            text-align: center;
+        }
+        .home-page-newbie-cards-wrapper {
+            justify-content: center;
+        }
+    }
+
+    .home-page-challange-title {
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin: 12px 0;
+        color: white;
+    }
+
+    .home-page-newbie-cards-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 12px;
+
+        .home-page-card {
+            min-width: 200px;
+            flex: 1;
+        }
+    
+
+    }
+}
+</style>
