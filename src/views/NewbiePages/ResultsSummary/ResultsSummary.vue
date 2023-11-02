ba912b3cf44a342a82e99f2f7a8124d7cf360887
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-11-02 21:48:58 +0200

    build separate component for skill category and score

--- src/views/NewbiePages/ResultsSummary/ResultsSummary.vue
+++ src/views/NewbiePages/ResultsSummary/ResultsSummary.vue
@@ -11,7 +11,19 @@
                 <p class="results-summary-card-score-info">Your performance exceed 65% of the people conducting the test here!</p>
             </div>
             <div class="results-summary-card-summary">
+                <div class="results-summary-card-content">
+                    <h3 class="results-summary-card-conten-title">Summary</h3>
+                    <ResultsSummarySkill
+                        v-for="skill in data"
+                        :key="skill.category"
+                        :category="skill.category"
+                        :score="skill.score"
+                        :iconPath="skill.icon"
+                        :colorClass="skill.colorClass"
+                    />
 
+                    <button class="results-summary-card-content-btn">Continue</button>
+                </div>
             </div>
         </div>
 
@@ -20,6 +32,7 @@
 </template>
 
 <script setup>
+import ResultsSummarySkill from './ResultsSummarySkill.vue';
 import data from './data.json'
 
 
@@ -28,6 +41,7 @@ import data from './data.json'
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;700;800&display=swap');
 $bg-color: #ECF2FF;
+$dark-navy-color: #303B59;
 $purple-light: #CAC9FF;
 $gradient-score-card: linear-gradient(180deg, #75F 0%, #6943FF 0.01%, #2F2CE9 100%);
 $gradient-score: linear-gradient(180deg, #4D21C9 0%, rgba(37, 33, 201, 0.00) 100%, rgba(37, 33, 201, 0.00) 100%);
@@ -35,7 +49,7 @@ $gradient-score: linear-gradient(180deg, #4D21C9 0%, rgba(37, 33, 201, 0.00) 100
     height: 100%;
     min-height: 100vh;
     width: 100%;
-    background-color: $bg-color;
+    background-color: white;
     font-family: 'Hanken Grotesk', sans-serif;
     font-size: 18px;
 }
@@ -94,11 +108,69 @@ $gradient-score: linear-gradient(180deg, #4D21C9 0%, rgba(37, 33, 201, 0.00) 100
             text-align: center;
             line-height: normal;
         }
-
     }
 
-    &-summary {
-        border: 3px solid greenyellow;
+    
+    .results-summary-card-summary {
+        background-color: white;
+        // padding: 0px 30px;
+
+        .results-summary-card-content {
+            width: 315px;
+            margin: auto;
+
+            .results-summary-card-conten-title {
+                color: $dark-navy-color;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: normal;
+                margin: 24px 0 24px;
+            }
+
+            .results-summary-card-content-btn {
+                width: 100%;
+                margin: 24px 0 30px;
+                padding: 16px;
+                background-color: $dark-navy-color;
+                color: white;
+                border: none;
+                border-radius: 128px;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: normal;
+
+                &:hover {
+                    cursor: pointer;
+                    background: $gradient-score-card;
+                }
+            }
+        }
+    }
+}
+
+@media screen and (min-width: 768px) {
+    .results-summary-page {
+        background-color: $bg-color;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .results-summary-card {
+        display: flex;
+        border-radius: 32px;
+
+        &-score {
+            width: 368px;
+            border-top-right-radius: 32px;
+            border-top-left-radius: 32px;
+            z-index: 5;
+        }
+
+        &-summary {
+            margin-left: -30px;
+            width: 398px;
+        }
     }
 }
 
