d6abbc58ec28edb327aef80014baebdfc83a11cd
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-11-08 22:57:24 +0200

    implement main  3 column preview page and component for separate column

---
+++ src/views/NewbiePages/3ColumnPreview/3ColumnPreview.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="3-column-preview-page">
+        <ColumnPreviewCard 
+            v-for="cardData in cardsData"
+            :key="cardData.carType"
+            :cardData="cardData"
+        />
+    </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ColumnPreviewCard from './ColumnPreviewCard.vue';
+
+const cardsData = ref([
+    {
+        carType: 'Sedans',
+        description: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+        carIcon: '/assets/images/3ColumnPreview/icon-sedans.svg'
+    },
+    {
+        carType: 'SUVs',
+        description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+        carIcon: '/assets/images/3ColumnPreview/icon-suvs.svg'
+    },
+    {
+        carType: 'Luxury',
+        description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+        carIcon: '/assets/images/3ColumnPreview/icon-luxury.svg'
+    },
+])
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NewbiePages/3ColumnPreview/ColumnPreviewCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="column-preview-card">
+        <img :src="cardData.carIcon" alt="car type icon">
+        <h2>{{ cardData.carType }}</h2>
+        <p>{{ cardData.description }}</p>
+    </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+    cardData: {
+        typ: Object
+    }
+})
+
+const { cardData } = toRefs(props)
+
+</script>
