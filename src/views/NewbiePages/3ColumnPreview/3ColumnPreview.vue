ff0cd43c4035bf44c8fd50803ebb7fbde52dde6d
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-11-08 23:35:56 +0200

    implement layout for page and columns component

--- src/views/NewbiePages/3ColumnPreview/3ColumnPreview.vue
+++ src/views/NewbiePages/3ColumnPreview/3ColumnPreview.vue
@@ -1,10 +1,12 @@
 <template>
-    <section class="3-column-preview-page">
-        <ColumnPreviewCard 
-            v-for="cardData in cardsData"
-            :key="cardData.carType"
-            :cardData="cardData"
-        />
+    <section class="columns-preview-page">
+        <div class="columns-preview">
+            <ColumnPreviewCard 
+                v-for="cardData in cardsData"
+                :key="cardData.carType"
+                :cardData="cardData"
+            />
+        </div>
     </section>
 </template>
 
@@ -16,23 +18,39 @@ const cardsData = ref([
     {
         carType: 'Sedans',
         description: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
-        carIcon: '/assets/images/3ColumnPreview/icon-sedans.svg'
+        carIcon: '/assets/images/3ColumnPreview/icon-sedans.svg',
+        cardClasses: 'sedan-card'
     },
     {
         carType: 'SUVs',
         description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-        carIcon: '/assets/images/3ColumnPreview/icon-suvs.svg'
+        carIcon: '/assets/images/3ColumnPreview/icon-suvs.svg',
+        cardClasses: 'suvs-card'
     },
     {
         carType: 'Luxury',
         description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
-        carIcon: '/assets/images/3ColumnPreview/icon-luxury.svg'
+        carIcon: '/assets/images/3ColumnPreview/icon-luxury.svg',
+        cardClasses: 'luxury-card'
     },
 ])
 
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped >
+@import url('https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&family=Lexend+Deca&display=swap');
+.columns-preview-page {
+    height: 100%;
+    min-height: 100vh;
+    width: 100%;
+    font-size: 15px;
+    font-family: 'Lexend Deca', sans-serif;
 
+    .columns-preview {
+        width: 327px;
+        margin: 88px auto;
+        border-radius: 8px;
+    }
+}
 </style>
