41d6c6690cf52c8271c8207724ab0e97730d6311
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-11-12 15:52:03 +0200

    implement base structure of card and functinality connection between components

--- src/views/NewbiePages/InteractiveRatingCard/InteractiveRatingCardView.vue
+++ src/views/NewbiePages/InteractiveRatingCard/InteractiveRatingCardView.vue
@@ -1,3 +1,53 @@
 <template>
-    InteractiveRatingCard
+    <section class="interactive-rating-card-page">
+        <div class="interactive-rating-card-wrapper">
+            <InteractiveRatingCardVote 
+                :isRated="!rating"
+                @vote="vote"
+            />
+        </div>
+    </section>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+import InteractiveRatingCardVote from './InteractiveRatingCardVote.vue'
+
+const rating = ref(null)
+
+function vote(voteValue) {
+    console.log(voteValue)
+    // rating.value = voteValue
+}
+
+</script>
+
+<style lang="scss" scoped>
+$body-bg: #131518;
+$card-bg: radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%);
+
+* {
+    box-sizing: border-box;
+}
+
+.interactive-rating-card-page {
+    height: 100%;
+    min-height: 100vh;
+    width: 100%;
+    background-color: $body-bg;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .interactive-rating-card-wrapper {
+        width: 327px;
+        height: 360px;
+        background: $card-bg;
+        border-radius: 15px;
+
+        padding: 24px 24px 32px;
+    }
+}
+
+</style>
---
+++ src/views/NewbiePages/InteractiveRatingCard/InteractiveRatingCardVote.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="interactive-rating-card-content-wrpaper">
+        <template v-if="!rating">
+            {{ isRated }}
+            <button @click="vote">Submit</button>
+        </template>
+    </div>
+</template>
+
+<script setup> 
+import { toRefs } from 'vue'
+
+const emit = defineEmits(['vote'])
+
+const props = defineProps({
+    isRated: {
+        type: Boolean,
+    }
+})
+const { rating } = toRefs(props)
+
+function vote() {
+}
+
+</script>
