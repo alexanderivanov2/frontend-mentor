cf552d0299f63cd27470fe41d0ca00dbb0fdd582
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-11-14 23:16:36 +0200

    refactor card border radius

--- src/views/NewbiePages/InteractiveRatingCard/InteractiveRatingCardView.vue
+++ src/views/NewbiePages/InteractiveRatingCard/InteractiveRatingCardView.vue
@@ -55,7 +55,8 @@ $card-bg: radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%);
         width: 412px;
         height: 416px;
         padding: 32px;
-        margin-top: 2px;
+        margin-top: 5px;
+        border-radius: 30px;
     }
 }
 
