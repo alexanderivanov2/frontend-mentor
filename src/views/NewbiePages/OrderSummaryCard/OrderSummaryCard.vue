5fc97b08e81c0bacfcd3194b427c52b85618e9d3
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-02-16 00:02:50 +0200

    add svgs and base background for mobile

--- src/views/NewbiePages/OrderSummaryCard/OrderSummaryCard.vue
+++ src/views/NewbiePages/OrderSummaryCard/OrderSummaryCard.vue
@@ -1,12 +1,31 @@
 <template>
     <section id="orderSummaryCardPage">
-        <h1>ORDER SUMMARY</h1>
+        <div class="order-summary-card">
+
+
+        </div>
     </section>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
+#orderSummaryCardPage {
+    $body-bg: #E0E8FF;
 
+    background-color: $body-bg;
+    min-height: 100vh;
+    height: 100%;
+    position: relative;
+
+    &::before {
+        content: url('../../../../public/assets/images/orderSummaryCard/pattern-background-mobile.svg');
+        top: 0;
+        position: absolute;
+
+    }
+
+
+}
 
 </style>
