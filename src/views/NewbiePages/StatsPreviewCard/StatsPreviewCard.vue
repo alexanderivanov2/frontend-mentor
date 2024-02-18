a153db2b76fba6efaaf2a33e8142c68f5b7d9073
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-02-18 18:48:37 +0200

    refactor

--- src/views/NewbiePages/StatsPreviewCard/StatsPreviewCard.vue
+++ src/views/NewbiePages/StatsPreviewCard/StatsPreviewCard.vue
@@ -211,7 +211,7 @@ onMounted(() => {
 
                 .card-information-statistics {
                     max-width: 403px;
-                    margin-top: 75px;
+                    margin-top: 80px;
                     flex-direction: row;
                     justify-content: start;
                     gap: 65px;
