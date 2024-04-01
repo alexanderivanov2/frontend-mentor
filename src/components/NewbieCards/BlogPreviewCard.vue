49497352bebc83e51edc0bceaa905fc894740ea5
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-04-01 23:12:52 +0300

    implement full structure of card

--- src/components/NewbieCards/BlogPreviewCard.vue
+++ src/components/NewbieCards/BlogPreviewCard.vue
@@ -1,3 +1,24 @@
 <template>
-    BLOG PREVIEW CARD
+    <div class="blog-preview-card">
+        <div class="blog-preview-card-image-wrapper">
+            <img :src="BlogPreviewImage" alt="blog preview image" />
+        </div>
+        <div class="blog-preview-card-body">
+            <div class="card-body-tags">
+                <span class="card-body-tag">Learing</span>
+            </div>
+            <p class="card-body-date">Published 21 Dec 2023</p>
+            <h3 class="card-body-title">HTML & CSS foundations</h3>
+            <p class="card-body-description">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+        </div>
+        <div class="blog-preview-card-footer">
+            <img :src="PublisherImage" alt="publisher avatar" class="publisher-avatar">
+            <p class="publisher-name">Greg Hooper</p>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import BlogPreviewImage from '../../../public/assets/images/blogPreviewCard/illustration-article.svg';
+import PublisherImage from '../../../public/assets/images/blogPreviewCard/image-avatar.webp';
+</script>
