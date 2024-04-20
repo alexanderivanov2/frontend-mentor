a57c76c783eaf83439d9e372b4e992d002feace3
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-04-20 13:17:47 +0300

    add image urls for challange cards

--- src/assets/constants/homePageConstants/newbieCardsConstants.js
+++ src/assets/constants/homePageConstants/newbieCardsConstants.js
@@ -6,51 +6,61 @@ const newbieCardsList = [
     {
         cardName: 'Blog Preview Card',
         routeName: 'BlogPreviewCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/blog-preview-card-challange.webp',
         difficulty: 1,    
     },
     {
         cardName: 'Social Links Profile',
         routeName: 'SocialLinksProfileCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/social-links-profile-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'Stats Preview Card',
         routeName: 'StatsPreviewCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/stats-preview-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'Order Summary Card',
         routeName: 'OrderSummaryCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/order-summary-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'NFT Preview Card',
         routeName: 'NFTPreviewCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/nft-preview-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'Interactive Rating Card',
         routeName: 'InteractiveRatingCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/interactive-rating-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'Three Column Preview',
         routeName: 'ThreeColumnPreview',
+        cardImgURL: 'src/assets/images/challangeCardImages/3-column-preview-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'Results Summary',
         routeName: 'ResultsSummary',
+        cardImgURL: 'src/assets/images/challangeCardImages/results-summary-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'Product Preview Card',
         routeName: 'ProductPreviewCard',
+        cardImgURL: 'src/assets/images/challangeCardImages/product-preview-card-challange.webp',
         difficulty: 1,
     },
     {
         cardName: 'QRCode',
         routeName: 'NewbieQRCode',
+        cardImgURL: 'src/assets/images/challangeCardImages/qr-code-card-challange.webp',
         difficulty: 1,    
     },
 ]
