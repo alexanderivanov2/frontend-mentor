1b022177b9d483a9bd6e2c23b1a15d7309d0d348
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-04-20 13:28:33 +0300

    implement challange card and embla coursel for them

---
+++ src/components/ChallangeCardCarousel/ChallangeCard.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="challangeCard"
+    :style="{
+        backgroundImage: `url(${imgUrl})`,
+        height: '200px',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+    }"
+    >
+        <h3 class="challange-card-title">{{ title }}</h3>
+        <span class="challange-card-difficulty">{{ getDifficulty }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, toRefs } from 'vue';
+
+interface Props {
+    title: string;
+    imgUrl: string;
+    difficulty: number | string;
+}
+
+interface DifficultyDictionary {
+    [key: string | number]: string;
+}
+
+const props = defineProps<Props>()
+const { difficulty } = toRefs(props)
+
+const difficultyDictionary: DifficultyDictionary = {
+    1: 'Newbie',
+    2: 'Junior',
+}
+const getDifficulty = computed(() => difficultyDictionary[difficulty.value] ?? 'unknown')
+</script>
+
+<style scooped lang="scss">
+    #challangeCard {
+        position: relative;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        letter-spacing: 1px;
+
+        .challange-card-title {
+            font-size: 1.6rem;
+            font-weight: 900;
+            color: white;
+
+            position: absolute;
+            bottom: 5%;
+            left: 0;
+            width: 100%;
+            text-align: center;
+            text-shadow: 1px 1px 1px rgba(0, 0, 0, .95);
+        }
+
+        .challange-card-difficulty {
+            position: absolute;
+            top: 5%;
+            right: 5%;
+            color: white;
+            background-color: black;
+            padding: 0.75rem 1.5rem;
+            font-size: 1.2rem;
+            font-weight: 900;
+            border-radius: 100px;
+        }
+    }
+</style>
--- src/components/ChallangeCardCarousel/ChallangeCardCarousel.vue
+++ src/components/ChallangeCardCarousel/ChallangeCardCarousel.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="embla" ref="emblaRef">
         <div class="embla__container">
-            <div class="embla__slide">Slide 1</div>
-            <div class="embla__slide">Slide 2</div>
-            <div class="embla__slide">Slide 3</div>
+            <div class="embla__slide" v-for="card in newbieCardsList">
+                <ChallangeCard 
+                  :title="card.cardName"
+                  :imgUrl="card.cardImgURL"
+                  :difficulty="card.difficulty"
+                />
+            </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import emblaCarouselVue from 'embla-carousel-vue'
+import ChallangeCard from './ChallangeCard.vue';
+import { newbieCardsList } from '../../assets/constants/homePageConstants/newbieCardsConstants' 
 
 const [emblaRef] = emblaCarouselVue()
 </script>
@@ -17,13 +23,32 @@ const [emblaRef] = emblaCarouselVue()
 <style scoped>
   .embla {
     overflow: hidden;
-    background-color: wheat;
+    max-width: 1920px;
+    margin: 0 auto;
+    /* background-color: wheat; */
   }
   .embla__container {
     display: flex;
+    /* flex-direction: column; */
+    height: 200px;
   }
   .embla__slide {
-    flex: 0 0 100%;
+    flex: 0 0 80%;
     min-width: 0;
+    margin-right: 15px;
+    
+    @media screen and (min-width: 550px) {
+        flex: 0 0 40%;
+    }
+
+    @media screen and (min-width: 900px) {
+        flex: 0 0 33%;
+    }
+
+    
+    @media screen and (min-width: 1200px) {
+        flex: 0 0 15%;
+    }
+    /* max-width: 200px; */
   }
 </style>
