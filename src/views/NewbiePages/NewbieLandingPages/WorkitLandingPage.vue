f5d27a874f4592bf6a6bac5d4e805c05513c7ae9
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-06-05 22:47:00 +0300

    mobile header part implemented

--- src/views/NewbiePages/NewbieLandingPages/WorkitLandingPage.vue
+++ src/views/NewbiePages/NewbieLandingPages/WorkitLandingPage.vue
@@ -1,3 +1,25 @@
 <template>
-    
+    <header class="workit-header">
+        <div class="workit-header-top">
+            <img :src="workitLogoLight" alt="workit tag">
+            <a href="#" class="workit-apply-link">Apply for access</a> 
+        </div>
+        <div class="workit-header-top-body">
+            <h1 class="workit-header-top-body-title">Data <span class="underline">tailred</span> to your needs</h1>
+            <button class="workit-header-top-body-btn-learn-more shine-btn-effect">Learn more</button>
+        </div>
+    </header>
+    <section class="benefits-section">
+        <div class="benefits-section-img-wrapper">
+            <img :src="mobileImageHero" alt="image of mobile phone" class="benefits-section-img">
+        </div>
+
+    </section>
+
 </template>
+
+<script setup>
+import workitLogoLight from '/assets/images/LandingPages/WorkitLandingPage/logo-light.svg'
+import mobileImageHero from '/assets/images/LandingPages/WorkitLandingPage/image-hero.webp'
+
+</script>
