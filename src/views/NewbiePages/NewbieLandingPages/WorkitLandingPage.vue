9c569793f7eda78f2af035ecde1301506efb610b
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-06-09 23:13:54 +0300

    add overlap image on header section for all devices

--- src/views/NewbiePages/NewbieLandingPages/WorkitLandingPage.vue
+++ src/views/NewbiePages/NewbieLandingPages/WorkitLandingPage.vue
@@ -1,18 +1,23 @@
 <template>
     <header class="workit-header">
-        <div class="workit-header-top">
-            <img :src="workitLogoLight" alt="workit tag">
-            <a href="#" class="workit-apply-link">Apply for access</a> 
-        </div>
-        <div class="workit-header-top-body">
-            <h1 class="workit-header-top-body-title">Data <span class="underline">tailred</span> to your needs</h1>
-            <button class="workit-header-top-body-btn-learn-more shine-btn-effect">Learn more</button>
+        <div class="workit-header-wrapper">
+            <div class="workit-header-top">
+                <img :src="workitLogoLight" alt="workit tag">
+                <a href="#" class="workit-apply-link">Apply for access</a>
+            </div>
+            <div class="workit-header-top-body">
+                <h1 class="workit-header-top-body-title">Data <span class="underline">tailred</span> to your needs</h1>
+                <button class="workit-header-top-body-btn-learn-more shine-btn-effect">Learn more</button>
+            </div>
+            <div class="workit-header-img-wrapper">
+                <img :src="mobileImageHero" alt="image of mobile phone" class="workit-header-img">
+            </div>
         </div>
     </header>
     <section class="benefits-section">
-        <div class="benefits-section-img-wrapper">
+        <!-- <div class="benefits-section-img-wrapper">
             <img :src="mobileImageHero" alt="image of mobile phone" class="benefits-section-img">
-        </div>
+        </div> -->
 
     </section>
 
