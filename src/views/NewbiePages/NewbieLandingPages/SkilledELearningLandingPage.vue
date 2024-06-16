3bcb8a2ead6fce02db4915dac3a36bd4038e1c7f
Alexander Ivanov <alexanderivanov2277@gmail.com>
2024-06-16 22:10:39 +0300

    implement hero header section

--- src/views/NewbiePages/NewbieLandingPages/SkilledELearningLandingPage.vue
+++ src/views/NewbiePages/NewbieLandingPages/SkilledELearningLandingPage.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="skilled-e-learning-landing-page">
+    <div class="skilled-e-learining-page-wrapper">
+        <div class="skilled-e-learning-landing-page">
             <header class="skilled-e-learning-header">
                 <img :src="logoDark" alt="dark logo of skilled">
                 <button class="btn btn-dark-blue">Get Started</button>
@@ -8,20 +9,36 @@
                 <div class="header-section-intro">
                     <h2 class="header-section-intro-title">Maximize skill,
                         minimize budget</h2>
-                    <p class="header-section-intro-description">Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.</p>
+                    <p class="header-section-intro-description">Our modern courses across a range of in-demand skills will
+                        give you the knowledge you need to live the life you want.</p>
                     <button class="btn btn-light-gradient">Get Started</button>
                 </div>
                 <div class="header-section-image-wrapper">
-                    <img :srcset="`${imageHero1x} 1x, ${imageHero2x} 2x`" alt="">
+                    <div class="responsive-background"></div>
                 </div>
             </section>
+    
+        </div>
     </div>
 </template>
 
 <script setup>
 import logoDark from '/assets/images/LandingPages/skilledELearningPage/logo-dark.svg'
 import logoLight from '/assets/images/LandingPages/skilledELearningPage/logo-light.svg'
-import imageHero1x from '/assets/images/LandingPages/skilledELearningPage/image-hero-mobile.webp'
-import imageHero2x from '/assets/images/LandingPages/skilledELearningPage/image-hero-mobile@2x.png'
+// import imageHeroMobile1x from '/assets/images/LandingPages/skilledELearningPage/image-hero-mobile.webp'
+// import imageHeroMobile2x from '/assets/images/LandingPages/skilledELearningPage/image-hero-mobile@2x.png'
+// import imageHeroTablet1x from '/assets/images/LandingPages/skilledELearningPage/image-hero-tablet.webp'
+// import imageHeroTablet2x from '/assets/images/LandingPages/skilledELearningPage/image-hero-tablet@2x.png'
+// import imageHeroDesktop1x from '/assets/images/LandingPages/skilledELearningPage/image-hero-desktop.webp'
+// import imageHeroDesktop2x from '/assets/images/LandingPages/skilledELearningPage/image-hero-desktop@2x.png'
+
+// const images = {
+//   mobile1x: imageHeroMobile1x,
+//   mobile2x: imageHeroMobile2x,  // 2x version for high-density displays
+//   tablet1x: imageHeroTablet1x,
+//   tablet2x: imageHeroTablet2x,  // High-resolution tablets
+//   desktop1x: imageHeroDesktop1x,
+//   desktop2x: imageHeroDesktop2x // High-resolution desktops
+// };
 
 </script>
