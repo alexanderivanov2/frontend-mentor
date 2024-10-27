284cacdfedb41268c8543898ba33782ca80d8369
Alexander Ivanov <mwpx123@gmail.com>
2024-10-28 00:25:19 +0200

    refactoring

--- src/components/Base/BaseInput.vue
+++ src/components/Base/BaseInput.vue
@@ -9,11 +9,11 @@
 </template>
 
 <script setup lang="ts">
-import { Ref } from 'vue'
+import { BaseInputType } from '../../types/inputTypes';
 interface Props {
     id: string,
     type: string,
-    modelValue: string | Ref<string>,
+    modelValue: string | BaseInputType,
     placeHolder?: string,
     useVModel?: boolean,
 }
@@ -29,58 +29,3 @@ const handleInput = (e: Event) => {
     emit('update:modelValue', (e.target as HTMLInputElement).value)
 }
 </script>
-
-
-<!-- 
-const validateInput = (value: string, data?: string | null) => {
-    if (value.length > props.max && data) return false  
-
-    if (props.validate && value) {
-        return props.validate.test(value)
-    }
-
-    return true
-}
-
-const handleInput = (e: InputEvent | Event ) => {
-    const value = (e?.currentTarget as HTMLInputElement)?.value
-
-    if (props.type === 'radio') {
-        emit('update:modelValue', value)
-        return 
-    }
-
-    const data = e instanceof InputEvent ? e?.data : '' 
-
-    const isValidInput = validateInput(value, data)
-    
-    if (props.strict) {
-        if(isValidInput) {
-            emit('update:modelValue', value)
-        } else if(input.value) {
-            input.value.value = props.modelValue ? props.modelValue : null
-        }
-    } else {
-        emit('update:modelValue', value)
-    }
-    emit('isValid', isValidInput)
-}
-
-const handleBlur = (e: Event) => {
-    if (props.formater && typeof props.formater == 'function') {
-        const value = props.modelValue && props.formater(props.modelValue) 
-        emit('update:modelValue', value)
-        if (input.value && value) {
-            input.value.value = value
-        }
-    }
-    const value = (input.value && input.value.value) || ''
-    const isValidInput = validateInput(value)
-    emit('blurInput', props.id, props.modelValue)
-    emit('isValid', isValidInput)
-} 
-
-const handleFocus = (e: Event) => {
-    emit('focusInput', props.id, props.modelValue)
-}
-</script> -->
