6cc0eec321eb0a0510569bbf6ee3f6d2b403b64e
Alexander Ivanov <mwpx123@gmail.com>
2024-10-28 00:28:31 +0200

    implement result component

--- src/views/JuniorPages/MortgageRepaymentCalculator/MortgageRepaymentResult.vue
+++ src/views/JuniorPages/MortgageRepaymentCalculator/MortgageRepaymentResult.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mortgage-repayment-calculator-result">
 
-        <div class="mortgage-repayment-calculator-result-no-result" v-if="!mortgageResult">
+        <div class="mortgage-repayment-calculator-result-no-result" v-if="!showResult">
             <img :src="emptyResultImg" alt="img for empty result">
             <h3 class="mortgage-repayment-calculator-result-no-result-heading">Results show here</h3>
             <p class="mortgage-repayment-calculator-result-no-result-description">Complete the form and click “calculate
@@ -14,13 +14,13 @@
                 again.</p>
             <div class="mortgage-repayment-calculator-result-calculations-repays">
                 <div class="monthly">
-                    <p class="monthly-heading">Your monthly repayments</p>
-                    <p class="monthly-repayment"> ${{ monthlyRepay }}</p>
+                    <p class="monthly-heading">Your monthly {{ mortgageType }}</p>
+                    <p class="monthly-repayment"> ${{ monthlyValue }}</p>
                 </div>
                 <div class="break-line"></div>
                 <div class="total">
-                    <p class="total-heading">Your monthly repayments</p>
-                    <p class="total-repayment"> ${{ totalRepay }}</p>
+                    <p class="total-heading">Total you'll {{ mortgageType }} over the term</p>
+                    <p class="total-repayment"> ${{ totalValue }}</p>
                 </div>
             </div>
         </div>
@@ -30,22 +30,17 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
 import emptyResultImg from '/assets/images/juniorChallenges/mortgage-repayment-calculator/illustration-empty.svg'
+import { mortgageResultType } from '../../../types/mortgageTypes';
+import { computed } from 'vue';
 
 interface Props {
-    mortgageResult: string;
+    mortgageResult: mortgageResultType;
     mortgageType: string;
-    mortgageTerm: string;
 }
 
-const props = withDefaults(defineProps<Props>(), {
-    mortgageResult: '',
-    mortgageType: '',
-    mortgageTerm: '',
-})
-
-const monthlyRepay = computed(() => Number(props.mortgageResult).toFixed(2))
-const totalRepay = computed(() => (Number(props.mortgageResult) * (Number(props.mortgageTerm) * 12)).toFixed(2))
-
+const props = defineProps<Props>()
+const showResult = computed<Boolean>(() => !!props.mortgageResult.monthly)
+const monthlyValue = computed<string>(() => Intl.NumberFormat("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2}).format(props.mortgageResult.monthly))
+const totalValue = computed<string>(() => Intl.NumberFormat("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2}).format(props.mortgageResult.total))
 </script>
