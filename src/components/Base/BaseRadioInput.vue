eee81364b63a014e6715c2d5b3531a253145c90a
Alexander Ivanov <mwpx123@gmail.com>
2024-10-27 16:25:23 +0200

    implement base radio input component

--- src/components/Base/BaseRadioInput.vue
+++ src/components/Base/BaseRadioInput.vue
@@ -1,3 +1,37 @@
 <template>
-    
+    <div id="RadioInputField">
+        RESULT: {{  modelValue }}
+        <label :for="id+'Lable'"> {{ label }}</label>
+        <div v-for="(value, key) in radioInputs" :key="value">
+            <label :for="value">{{ value }}</label>
+            <BaseInput
+                :id="value"
+                type="radio"
+                :name="value"
+                :value="value"
+                :checked="value === modelValue"
+                :model-value="modelValue"
+                @input="updateValue"
+            />
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+import BaseInput from './BaseInput.vue';
+
+interface Props {
+    id: string;
+    label: string;
+    modelValue?: string;
+    radioInputs: string[];
+}
+
+defineProps<Props>()
+const emits = defineEmits(['update:modelValue'])
+
+const updateValue = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  emits('update:modelValue', target.value);
+}
+</script> 
