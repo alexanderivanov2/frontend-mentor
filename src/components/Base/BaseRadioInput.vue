d737e80ce92d061277b7ceb73e96b795b5cca804
Alexander Ivanov <mwpx123@gmail.com>
2024-10-28 00:27:15 +0200

    implement base radio button

--- src/components/Base/BaseRadioInput.vue
+++ src/components/Base/BaseRadioInput.vue
@@ -1,37 +1,47 @@
 <template>
     <div id="RadioInputField">
-        RESULT: {{  modelValue }}
         <label :for="id+'Lable'"> {{ label }}</label>
-        <div v-for="(value, key) in radioInputs" :key="value">
+        <div v-for="(value, key) in radioInputs" :key="value"
+            class="radio-input-wrapper"
+        >
             <label :for="value">{{ value }}</label>
             <BaseInput
                 :id="value"
                 type="radio"
                 :name="value"
                 :value="value"
-                :checked="value === modelValue"
-                :model-value="modelValue"
+                :checked="value === input.value"
+                :model-value="input"
                 @input="updateValue"
             />
         </div>
+        <BaseErrorMessage v-if="!input.isValid">
+            <p class="error-message">{{ input.errorMessage }}</p>
+        </BaseErrorMessage>
     </div>
 </template>
 
 <script setup lang="ts">
+import { BaseInputType } from '../../types/inputTypes';
 import BaseInput from './BaseInput.vue';
+import BaseErrorMessage from './BaseErrorMessage.vue';
 
 interface Props {
     id: string;
     label: string;
-    modelValue?: string;
+    input: BaseInputType;
     radioInputs: string[];
 }
 
-defineProps<Props>()
-const emits = defineEmits(['update:modelValue'])
+const props = defineProps<Props>()
 
 const updateValue = (event: Event) => {
   const target = event.target as HTMLInputElement;
-  emits('update:modelValue', target.value);
+  props.input.value = target.value
+
+  if (props.input?.isValid === false) {
+    props.input.isValid = true
+    props.input.errorMessage = ''
+  }
 }
 </script> 
