73f27af6c8f3d6d17bde2af3e8a54c46b7405898
Alexander Ivanov <mwpx123@gmail.com>
2024-10-27 16:22:54 +0200

    implement input handlers composables

---
+++ src/composables/useInputHandlers.ts
@@ -0,0 +1,65 @@
+import { Ref, ref } from "vue";
+import { BaseInput, BaseInputConfig } from "../types/inputTypes";
+
+const useInputHandlers = () => {
+    const createBaseInput = () => {
+        return ref<BaseInput>({
+            value: '',
+            isValid: true,
+            errorMessage: '',
+        })
+    }
+    
+    const handleInput =
+        (input: Ref<BaseInput>, config?: BaseInputConfig) => (e: Event) => {
+            const inputElement = e.currentTarget as HTMLInputElement;
+            const value = inputElement.value;
+            const validator = config?.validator;
+            const isValid = validator ? validator(value) : true;
+
+            if (config?.strict) {
+                if (isValid) {
+                    input.value.value = value;
+                }
+            }
+
+            if (!isValid && config?.errorHandling) {
+                input.value.isValid = false;
+                input.value.errorMessage = config?.errorHandling.errorMessage;
+            } else if (!input.value.isValid) {
+                input.value.isValid = true;
+                input.value.errorMessage = "";
+            }
+
+            inputElement.value = config?.strict ? input.value.value : value;
+        };
+
+
+    const handleFocusInputIntlDeformat = (input: Ref<BaseInput>) => (e: Event) => {
+        const inputElement = e.currentTarget as HTMLInputElement
+        if (input.value.isValid) {
+            inputElement.value = inputElement.value.replaceAll(',', '')
+        }
+
+        if(!input.value.value) {
+            inputElement.value = ''
+        }
+    };
+
+    const handleBlurInputIntlFormat = (input: Ref<BaseInput>) => (e: Event) => {
+        const inputElement = e.currentTarget as HTMLInputElement
+        if (input.value.isValid) {
+            inputElement.value = Intl.NumberFormat("en-US").format(Number(inputElement.value))
+
+        }
+    };
+
+    return {
+        handleInput,
+        handleFocusInputIntlDeformat,
+        handleBlurInputIntlFormat,
+        createBaseInput,
+    };
+};
+
+export { useInputHandlers };
